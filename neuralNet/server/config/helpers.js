90340c89bce9f6e426f73367451f0165d47eac31
Katrina Uychaco <kuychaco@gmail.com>
2015-04-14 22:45:38 -0700

    Implement neural network. Woot.

---
+++ neuralNet/server/config/helpers.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = {
+  errorLogger: function(error, req, res, next) {
+    // log the error then send it to the next middleware in middleware.js
+    console.error(error.stack);
+    next(error);
+  },
+  errorHandler: function(error, req, res, next) {
+    // send error message to the client
+    // message for graceful error handling on app
+    res.status(500).send({error: error.message});
+  }
+};
